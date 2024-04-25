c037b191bde70533a4e92ae6e550e8872e5a4053
nblexi <redacted>
2024-04-24 19:27:18 -0500

    youtube-old-layout

--- remove_comments.user.js
+++ remove_comments.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         remove youtube comments
-// @version      0.8.3
+// @version      0.8.4
 // @description  youtube comments are a cesspool
 // @author       lexi
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @downloadURL  https://raw.githubusercontent.com/nblexi/youtube-scripts/main/comments.user.js
-// @updateURL    https://raw.githubusercontent.com/nblexi/youtube-scripts/main/comments.user.js
+// @downloadURL  https://raw.githubusercontent.com/nblexi/youtube-scripts/main/remove_comments.user.js
+// @updateURL    https://raw.githubusercontent.com/nblexi/youtube-scripts/main/remove_comments.user.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
@@ -17,14 +17,14 @@ let commentsvisible = true;
 
 let comment_visibility = () => {
   if (commentsvisible) {
-    comments_section = document.querySelector("#comments");
+    comments_section = document.querySelector('#comments');
 
     if (!comments_section) {
       setTimeout(() => {
         comment_visibility();
       }, 2000);
     } else {
-      console.info("[comments] removed comments");
+      console.info('[comments] removed comments');
       commentsvisible = false;
       comments_section.remove();
     }
@@ -32,12 +32,12 @@ let comment_visibility = () => {
 };
 
 let main = () => {
-  "use strict";
+  'use strict';
   comment_visibility();
 };
 
 let site = () => {
-  "use strict";
+  'use strict';
   if (window.location.href != current_page) {
     window.history.__proto__.pushState = (a, b, url) => {
       console.debug(`[comments] state: ${a}`);
@@ -48,12 +48,12 @@ let site = () => {
   if (is_site()) {
     main();
   } else {
-    console.info("[comments] Ignore this page");
+    console.info('[comments] Ignore this page');
   }
 };
 
 let is_site = () => {
-  "use strict";
+  'use strict';
   if (window.location.href.match(/https?:\/\/www\.youtube\.com\/watch\.*/)) {
     return true;
   } else {
@@ -61,12 +61,12 @@ let is_site = () => {
   }
 };
 
-document.addEventListener("yt-navigate-finish", (e) => {
+document.addEventListener('yt-navigate-finish', (e) => {
   site();
 });
 
 (() => {
-  "use strict";
+  'use strict';
   window.history.__proto__.pushState = function (a, b, url) {
     window.location.href = url;
     current_page = url;
